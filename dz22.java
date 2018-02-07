3f49364a05cee9403836ae696e6cf8d00a245a77
soingleb <34136876+soingleb@users.noreply.github.com>
2018-02-07 19:51:47 +0200

    first

---
+++ dz22.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class dz22 {
+
+    public static void main(String[] args) {
+        System.out.println("vvedi 4islo bol'she 9 menishe 100");
+        Scanner number1 = new Scanner(System.in);
+        int n = number1.nextInt();
+        while (n<10||n>99) {
+            Scanner number2 = new Scanner(System.in);
+            n = number1.nextInt();
+        }
+        int a = n / 10 + n % 10;
+        System.out. println("Summa =" + a);
+    }
+}
